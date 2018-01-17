042228eaf56d6777dfaf5c398ffbcb2deacdc49b
liu.yongfeng <lyf198511@163.com>
2018-01-17 22:48:43 +0800

    定时器中断实验 更新

--- example/HAREWARE/TIMER/timer.c
+++ example/HAREWARE/TIMER/timer.c
@@ -22,26 +22,30 @@ void TIM3_Init(u16 arr, u16 psc ){
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
 	NVIC_InitTypeDef NVIC_InitStruct;
 	
+	//1、 能定时器时钟。
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
 	
+	//2、 初始化定时器，配置ARR,PSC。
 	TIM_TimeBaseInitStruct.TIM_Period = arr;
 	TIM_TimeBaseInitStruct.TIM_Prescaler = psc;
 	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInit(TIM3,&TIM_TimeBaseInitStruct);
 	
+	//3、开启定时器中断，配置NVIC。
 	TIM_ITConfig(TIM3,TIM_IT_Update,ENABLE);
-	
+
 	NVIC_InitStruct.NVIC_IRQChannel = TIM3_IRQn;
 	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
 	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 3;
 	NVIC_Init(&NVIC_InitStruct);
 	
+	//4、  使能定时器。
 	TIM_Cmd(TIM3,ENABLE);
 }
 
-
+//5、 编写中断服务函数。
 void TIM3_IRQHandler(void){
 
 	if(TIM_GetITStatus(TIM3,TIM_IT_Update) != RESET){
